71cdbbb5816fb58966cd0a75c8ca3bd810f51cb5
bobo0810 <1055271769@qq.com>
2020-11-02 17:29:13 +0800

    fixed bugs

--- TargetDrop.py
+++ TargetDrop.py
@@ -1,45 +1,19 @@
+import os
 import torch
 import torch.nn as nn
-from torch.nn import Linear, Conv2d, BatchNorm1d, BatchNorm2d, PReLU, ReLU, Sigmoid, Dropout, MaxPool2d, \
-    AdaptiveAvgPool2d, Sequential, Module
+from torch.nn import Conv2d, ReLU, Sigmoid, AdaptiveAvgPool2d, Module
 
-class SEModule_FC(Module):
+
+class SEModule(Module):
     '''
-    原生SE模块，提取通道注意力权重
-    '''
-    def __init__(self, channels, reduction,L=4):
-        super(SEModule_FC, self).__init__()
-        self.avg_pool = AdaptiveAvgPool2d(1)
-        d = max(channels // reduction, L)  # 计算向量Z 的长度d   下限为L
-        self.fc = nn.Sequential(
-            nn.Linear(channels, d, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(d, channels, bias=False),
-            nn.Sigmoid()
-        )
-        # 未初始化权重
-
-    def forward(self, x):
-        batch, channle,  _, _ = x.size()
-        x = self.avg_pool(x).view(batch, channle)
-        x = self.fc(x).view(batch, channle, 1, 1)
-        return x
-
-
-class SEModule_Conv(Module):
-    '''
-    SE模块提取通道注意力权重（Conv替代FC层）
+    SE模块 提取通道注意力权重（Conv替代FC层）
     '''
     def __init__(self, channels, reduction):
-        super(SEModule_Conv, self).__init__()
+        super(SEModule, self).__init__()
         self.avg_pool = AdaptiveAvgPool2d(1)
         self.fc1 = Conv2d(channels, channels // reduction, kernel_size=1, padding=0, bias=False)
-
-        torch.nn.init.xavier_uniform_(self.fc1.weight.data)
-
         self.relu = ReLU(inplace=True)
         self.fc2 = Conv2d(channels // reduction, channels, kernel_size=1, padding=0, bias=False)
-        # 未初始化fc2
         self.sigmoid = Sigmoid()
 
     def forward(self, x):
@@ -48,119 +22,125 @@ class SEModule_Conv(Module):
         x = self.relu(x)
         x = self.fc2(x)
         x = self.sigmoid(x)
-        return  x
+        return x
+
 
 class Select_TopK(Module):
     '''
-    根据 注意力权重，选择 TopK 对应的特征通道
+    选择 TopK 对应的特征通道
     '''
-    def __init__(self,top_k):
-        super(Select_TopK,self).__init__()
-        self.top_k=top_k # 屏蔽的通道数
+    def __init__(self, top_k):
+        super(Select_TopK, self).__init__()
+        self.top_k = top_k
 
     def forward(self, x):
-        # x∈[batch,channel,1,1]
         batch, channel, h, w = x.shape
         x = x.squeeze()
         # 通道维度 按权重降序时的下标值(索引)
-        topk_index = torch.argsort(x, dim=1, descending=True)[:,:self.top_k].long()
-        #  初始化全零
+        topk_index = torch.argsort(x, dim=1, descending=True)[:, :self.top_k].long()
         T = torch.zeros_like(x)
-
-        value = torch.ones_like(x)  # 填充值
-        # dim=1 行不变（依次递增）列变化   index:列索引值，用于变化列的值  src:填充值
-        T.scatter_(dim=1,index=topk_index,src=value)
+        T.scatter_(dim=1, index=topk_index, src=torch.ones_like(x))
         return T.reshape(batch, channel, h, w)
 
-class Local_Mask(Module):
+
+class Apply_Mask(Module):
     '''
     针对标记通道，生成 位置mask
     '''
-    def __init__(self,drop_block):
-        super(Local_Mask, self).__init__()
-        self.drop_block=torch.FloatTensor([drop_block]) # 屏蔽矩阵大小
-    def forward(self,x,T):
+    def __init__(self, drop_block):
+        super(Apply_Mask, self).__init__()
+        self.drop_block = torch.FloatTensor([drop_block])  # 屏蔽矩阵大小
+
+    def forward(self, x, T):
         '''
         :param x: 输入特征  [batch,channel,H,W]
         :param T: 标记通道T 0-1  [batch,channel,1,1]
         :return:
         '''
         batch, channel, h, w = x.shape
+        self.drop_block = self.drop_block.to(x.device)
         assert h >= self.drop_block.item() and w >= self.drop_block.item()
-        # ===========提取目标特征图x_mask===========
-        x=x.reshape(-1,h,w)
-        T=T.reshape(-1,1)
-        index = torch.nonzero(T, as_tuple=True)[0] #非0索引
-        x_mask=x[index]
-        # ===========每张特征图最大值索引max_index==========
-        x_mask_squeeze=x_mask.reshape(x_mask.shape[0],-1)
-        index=x_mask_squeeze.argmax(dim=1)
-        max_index = torch.cat(((index//h).unsqueeze(-1), (index % h).unsqueeze(-1)), 1)
-        # ===========生成kxk屏蔽矩阵S===========
-        S=torch.ones_like(x_mask)
-
-        max_index_h=max_index[:,0]
-        max_index_w=max_index[:, 1]
-
-        h1=max_index_h-(self.drop_block / 2.).floor().to(max_index_h.device)
-        h2=max_index_h+(self.drop_block / 2.).floor().to(max_index_h.device)
-        w1=max_index_w-(self.drop_block / 2.).floor().to(max_index_w.device)
-        w2 = max_index_w + (self.drop_block / 2.).floor().to(max_index_w.device)
-        h1, h2 = torch.clamp(torch.cat((h1.unsqueeze(0), h2.unsqueeze(0))), min=0, max=h - 1).int().cpu().numpy().tolist()  # 防止越界
-        w1, w2 = torch.clamp(torch.cat((w1.unsqueeze(0), w2.unsqueeze(0))), min=0, max=w - 1).int().cpu().numpy().tolist()
+        # 提取目标特征图x_mask
+        x = x.reshape(-1, h, w)
+        T = T.reshape(-1, 1)
+        index = torch.nonzero(T, as_tuple=True)[0]  # 非0索引
+        x_mask = x[index]
+        # 每张特征图最大值索引max_index
+        x_mask_squeeze = x_mask.reshape(x_mask.shape[0], -1)
+        index = x_mask_squeeze.argmax(dim=1)
+        max_index_h, max_index_w = index // w, index % w
+        # 生成kxk屏蔽矩阵S  公式4
+        h1 = max_index_h - (self.drop_block / 2.).floor()
+        h2 = max_index_h + (self.drop_block / 2.).floor()
+        w1 = max_index_w - (self.drop_block / 2.).floor()
+        w2 = max_index_w + (self.drop_block / 2.).floor()
+        h1, h2 = torch.clamp(torch.cat((h1.unsqueeze(0), h2.unsqueeze(0))), min=0, max=h - 1).int()
+        w1, w2 = torch.clamp(torch.cat((w1.unsqueeze(0), w2.unsqueeze(0))), min=0, max=w - 1).int()
+        S = torch.ones_like(x_mask)
         for i in range(len(S)):
-            S[i, h1[i]:h2[i], w1[i]:w2[i]] = 0
-
-        # 加权并规范化
-        S_reshape=S.reshape(S.shape[0],-1)
-        λ =S_reshape.shape[-1]/S_reshape.sum(dim=-1) # 规范化系数 公式（6）
-        x_mask=x_mask * S * λ.reshape(λ.shape[0],1,1)
-
+            S[i, h1[i]:h2[i] + 1, w1[i]:w2[i] + 1] = 0  # 公式5
+        # 加权并规范化  公式6
+        S_flatten = S.reshape(S.shape[0], -1)
+        λ = S_flatten.shape[-1] / S_flatten.sum(dim=-1)
+        x_mask = x_mask * S * λ.reshape(λ.shape[0], 1, 1)
         # 根据序号放回原特征图中
-        x[index]=x_mask
+        x[index] = x_mask
         return x.reshape(batch, channel, h, w)
+
+
 class TargetDrop(Module):
-    def __init__(self, channels,reduction=16,drop_prob=0.15,drop_block=5):
+    def __init__(self, channels, reduction=16, drop_prob=0.15, drop_block=5):
         '''
-        论文默认参数  reduction=16,drop_prob=0.15,drop_block=5
+        论文默认参数
         :param channels:   输入特征的通道数
         :param reduction:  SE通道注意力
-        :param drop_prob: 屏蔽通道数占总通道数的百分比
+        :param drop_prob:  屏蔽通道数占总通道数的百分比
         :param drop_block: 屏蔽矩阵大小
         '''
         super(TargetDrop, self).__init__()
-
-        # 提取通道注意力权重
-        self.SEModule_Conv = SEModule_Conv(channels,reduction)
-        # self.SEModule_FC = SEModule_FC(channels, reduction)
-
-        # 标记通道T 0-1
+        self.SEModule = SEModule(channels, reduction)
         self.Select_TopK = Select_TopK(int(channels * drop_prob))
+        self.Apply_Mask = Apply_Mask(drop_block)
+        self.initialize_weights()
 
-        # 针对标记通道，生成 位置mask
-        self.Local_Mask = Local_Mask(drop_block)
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
     def forward(self, x):
         '''
         :param x: 输入特征
         :return:
         '''
         if self.training:
-            # 通道注意力权重  M∈[batch,channel,1,1]
-            M = self.SEModule_Conv(x)
-            # 标记通道 0-1    T∈[batch,channel,1,1]
+            # 通道注意力权重 [batch,channel,1,1]
+            M = self.SEModule(x)
+            # 标记通道 0-1  [batch,channel,1,1]
             T = self.Select_TopK(M)
-            # 生成 位置mask   Mask_X∈[batch,channel,H,W]
-            return self.Local_Mask(x,T)
+            # [batch,channel,H,W]
+            return self.Apply_Mask(x, T)
         else:
             return x  # eval模式无需drop
 
-if __name__ == '__main__':
-    import os
-    os.environ['CUDA_VISIBLE_DEVICES'] = "1"
 
-    feature = torch.rand(8,64, 24,24).cuda()
-    target_drop = TargetDrop(channels=feature.shape[1]).cuda()
-    target_drop =target_drop.train() # 训练模式
+if __name__ == '__main__':
+    os.environ['CUDA_VISIBLE_DEVICES'] = "1"
+    device = torch.device("cuda:0")
+    # [batch,channel,H,W]
+    feature = torch.rand(8, 64, 24, 22).to(device)
+    channels = feature.shape[1]
+    target_drop = TargetDrop(channels=channels).to(device)
+    target_drop = target_drop.train()
 
     result = target_drop(feature)
     print(result.size())
